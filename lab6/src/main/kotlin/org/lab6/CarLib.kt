2032946c3e2da176d4c0be5970aab89224dc1895
Андреев Александр Иванович <ai_andreev@controlsystems.ru>
2025-01-26 19:54:43 +0300

    lab6

---
+++ lab6/src/main/kotlin/org/lab6/CarLib.kt
@@ -0,0 +1,36 @@
+package org.lab6
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CarLib {
+    var id: Int = 0
+    var make: String? = null
+    var model: String? = null
+    var year: Int = 0
+    var color: String? = null
+    var mileage: Int = 0
+
+    // Конструктор по умолчанию
+    constructor()
+
+    // Конструктор с аргументами
+    constructor(id: Int, make: String, model: String, year: Int, color: String, mileage: Int) {
+        this.id = id
+        this.make = make
+        this.model = model
+        this.year = year
+        this.color = color
+        this.mileage = mileage
+    }
+
+    override fun toString(): String {
+        return ("CarService {" + "Id=" + id
+                + "make=" + make +
+                ", model=" + model +
+                ", year=" + year +
+                ", color=" + color +
+                ", mileage=" + mileage +
+                '}')
+    }
+}
---
+++ lab6/src/main/kotlin/org/lab6/client/Client.kt
@@ -0,0 +1,196 @@
+package org.lab6.client
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+
+import kotlinx.coroutines.runBlocking
+import org.lab6.CarLib
+import java.util.*
+
+
+class CarServiceClientCLI(
+    private val client: HttpClient
+) {
+    fun run() {
+        val scanner = Scanner(System.`in`)
+
+        while (true) {
+            println("Выберите действие: a - Получить все машины, c - Добавить машину, r - Получить машину, u - Обновить машину, d - Удалить машину, e - Выйти")
+            val choice = scanner.nextLine()
+
+            when (choice) {
+                "a" -> {
+                    runBlocking {
+                        try {
+                            val cars: List<CarLib> = getAllCars()
+                            println("All cars")
+                            for (car in cars) {
+                                println(
+                                    ("Cars {" + "id=" + car.id
+                                            ).toString() + ", make=" + car.make.toString() +
+                                            ", model=" + car.model +
+                                            ", color=" + car.color +
+                                            ", mileage=" + car.mileage +
+                                            ", year=" + car.year + '}'
+                                )
+                            }
+                        } catch (e: Exception) {
+                            println("Произошла ошибка: ${e.message}")
+                        }
+                    }
+                }
+                "c" -> {
+                    // Логика добавления машины
+                    val car = inputCar()
+                    if (car == null) {
+                        println("Проверьте корректность введенных данных")
+                        continue
+                    }
+                    runBlocking {
+                        try {
+                            createCar(car)
+                            println("Машина создана")
+                        } catch (e: Exception) {
+                            println("Произошла ошибка: ${e.message}")
+                        }
+                        
+                    }
+                }
+                "r" -> {
+                    // Логика получения машины
+                    println("Введите ID машины:")
+                    val id = scanner.nextLine().toInt()
+                    runBlocking {
+                        try {
+                            val car = getCar(id)
+                            println("Машина с id $id: $car")
+                        } catch (e: Exception) {
+                            println("Произошла ошибка: ${e.message}")
+                        }
+                    }
+                }
+                "u" -> {
+                    // Логика обновления машины
+                    println("Введите ID машины для обновления:")
+                    val id = scanner.nextLine().toInt()
+
+                    val updatedCar = inputCar()
+                    if (updatedCar != null) {
+                        updatedCar.id = id
+                    } else continue;
+
+                    runBlocking {
+                        val success = updateCar(id, updatedCar)
+                        if (success) println("Машина обновлена успешно") else println("Ошибка при обновлении машины")
+                    }
+                }
+                "d" -> {
+                    // Логика удаления машины
+                    println("Введите ID машины для удаления:")
+                    val id = scanner.nextLine().toInt()
+                    runBlocking {
+                        val success = deleteCar(id)
+                        if (success) println("Машина удалена успешно") else println("Ошибка при удалении машины")
+                    }
+                }
+                "e" -> {
+                    println("Выход из программы...")
+                    return
+                }
+                else -> {
+                    println("Некорректный ввод. Попробуйте снова.")
+                }
+            }
+
+        }
+    }
+
+    fun inputCar(): CarLib? {
+        val scanner = Scanner(System.`in`)
+
+        // Запрашиваем данные для обновления
+        println("Введите марку машины:")
+        val make = scanner.nextLine()
+        println("Введите модель машины:")
+        val model = scanner.nextLine()
+        println("Введите цвет машины:")
+        val color = scanner.nextLine()
+        println("Введите пробег машины:")
+        val mileageInput = scanner.nextLine()
+        val mileage: Int;
+        println("Введите год машины:")
+        val yearInput = scanner.nextLine()
+        val year: Int;
+
+        try {
+            year = yearInput?.toInt() ?: throw NumberFormatException("Год не введен")
+        } catch (e: NumberFormatException) {
+            println("Ошибка: неверный формат года. Пожалуйста, введите число.")
+            return null
+        }
+
+        try {
+            mileage = mileageInput?.toInt() ?: throw NumberFormatException("Пробег не введен")
+        } catch (e: NumberFormatException) {
+            println("Ошибка: неверный формат пробега. Пожалуйста, введите число.")
+            return null
+        }
+
+        val car = CarLib(0, make, model, year, color, mileage) // Создаем объект машины
+        return car
+    }
+
+    suspend fun getAllCars(): List<CarLib> {
+        val cars: List<CarLib> = client.get("http://localhost:8080/cars").body()
+        return cars
+    }
+
+    private suspend fun getCar(id: Int): CarLib {
+        val car: CarLib = client.get("http://localhost:8080/cars/$id").body()
+        return car
+    }
+
+    private suspend fun createCar(car: CarLib): Boolean {
+        val response = client.post("http://localhost:8080/cars") {
+            contentType(ContentType.Application.Json)
+            setBody(car)
+        }
+        return response.status.value == 200
+    }
+
+    private suspend fun updateCar(id: Int, car: CarLib): Boolean {
+        val response = client.put("http://localhost:8080/cars/$id") {
+            contentType(ContentType.Application.Json)
+            setBody(car)
+        }
+        return response.status.value == 200
+    }
+
+    private suspend fun deleteCar(id: Int): Boolean {
+        val response = client.delete("http://localhost:8080/cars/$id")
+        return response.status.value == 200
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val client = HttpClient(CIO) {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }
+
+            val clientCLI = CarServiceClientCLI(client)
+            clientCLI.run()
+        }
+    }
+
+}
+
